433085ccc8418f6a79d4e77e17e3a6a5fe5c7505
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-03-02 11:36:31 +0000

    :zap: Add priority

--- cmd/add.go
+++ cmd/add.go
@@ -23,6 +23,8 @@ import (
 	"github.com/st3v3nhunt/tri/todo"
 )
 
+var priority int
+
 func init() {
 	rootCmd.AddCommand(addCmd)
 
@@ -35,6 +37,7 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	addCmd.Flags().IntVarP(&priority, "priority", "p", 2, "Priority:1,2,3")
 }
 
 // addCmd represents the add command
@@ -52,7 +55,9 @@ func addRun(cmd *cobra.Command, args []string) {
 	}
 
 	for _, x := range args {
-		items = append(items, todo.Item{Text: x})
+		item := todo.Item{Text: x}
+		item.SetPriority(priority)
+		items = append(items, item)
 	}
 
 	err = todo.SaveItems(datafile, items)
